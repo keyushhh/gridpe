436b8e3c1c863f6eb6e6a01a699746dd1880a09d
Biradhwaj <keyushh@gmail.com>
2026-06-25 16:31:04 +0530

    chore: remove deprecated file utilities and associated helper methods

--- src/lib/analytics/providers/sentry.ts
+++
@@ -1,8 +0,0 @@
-// Placeholder for Sentry crash reporting integration.
-// Install: npm install @sentry/capacitor @sentry/react
-// Docs: https://docs.sentry.io/platforms/javascript/guides/capacitor/
-//
-// import * as Sentry from '@sentry/capacitor';
-// export function initSentry() {
-//   Sentry.init({ dsn: import.meta.env.VITE_SENTRY_DSN });
-// }
